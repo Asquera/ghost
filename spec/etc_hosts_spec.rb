79a687157df4dace78f675905983e4a2316d4b10
Florian Gilcher <florian.gilcher@asquera.de>
2011-08-29 09:02:32 +0200

    Use shared specs

--- spec/etc_hosts_spec.rb
+++ spec/etc_hosts_spec.rb
@@ -1,63 +1,66 @@
-require 'ghost/linux-host'
+require 'spec_helper'
+require 'ghost/hosts_file'
 
 $hosts_file   = File.expand_path(File.join(File.dirname(__FILE__), "etc_hosts"))
 
-describe Host do
+describe Ghost::HostsFile do  
   before(:all) do
-    class Host
+    class Ghost::HostsFile
       @@hosts_file = $hosts_file
     end
   end
-  before { `touch #{$hosts_file.inspect}` }
+  before(:each) { `touch #{$hosts_file.inspect}` }
   after  { `rm -f #{$hosts_file.inspect}` }
+  
+  it_behaves_like "a Ghost host"
   
   it "has an IP" do
     hostname = 'ghost-test-hostname.local'
 
-    Host.add(hostname)
-    host = Host.list.first
+    Ghost::HostsFile.add(hostname)
+    host = Ghost::HostsFile.list.first
     host.ip.should eql('127.0.0.1')
 
-    Host.empty!
+    Ghost::HostsFile.empty!
 
     ip = '169.254.23.121'
-    host = Host.add(hostname, ip)
+    host = Ghost::HostsFile.add(hostname, ip)
     host.ip.should eql(ip)
   end
 
   it "has a hostname" do
     hostname = 'ghost-test-hostname.local'
 
-    Host.add(hostname)
-    host = Host.list.first
+    Ghost::HostsFile.add(hostname)
+    host = Ghost::HostsFile.list.first
     host.hostname.should eql(hostname)
 
-    Host.empty!
+    Ghost::HostsFile.empty!
 
     ip = '169.254.23.121'
-    Host.add(hostname, ip)
+    Ghost::HostsFile.add(hostname, ip)
     host.hostname.should eql(hostname)
   end
   
   describe ".list" do
     it "returns an array" do
-      Host.list.should be_instance_of(Array)
+      Ghost::HostsFile.list.should be_instance_of(Array)
     end
 
-    it "contains instances of Host" do
-      Host.add('ghost-test-hostname.local')
-      Host.list.first.should be_instance_of(Host)
+    it "contains instances of Ghost::HostsFile" do
+      Ghost::HostsFile.add('ghost-test-hostname.local')
+      Ghost::HostsFile.list.first.should be_instance_of(Ghost::HostsFile)
     end
     
     it "gets all hosts on a single /etc/hosts line" do
       example = "127.0.0.1\tproject_a.local\t\t\tproject_b.local   project_c.local"
       File.open($hosts_file, 'w') {|f| f << example}
-      hosts = Host.list
+      hosts = Ghost::HostsFile.list
       hosts.should have(3).items
-      hosts.map{|h|h.ip}.uniq.should == ['127.0.0.1']
-      hosts.map{|h|h.host}.sort.should == %w[project_a.local project_b.local project_c.local]
-      Host.add("project_d.local")
-      Host.list.should have(4).items
+      hosts.map{|h|h.ip}.uniq.should eql(['127.0.0.1'])
+      hosts.map{|h|h.host}.sort.should eql(%w[project_a.local project_b.local project_c.local])
+      Ghost::HostsFile.add("project_d.local")
+      Ghost::HostsFile.list.should have(4).items
     end
   end
   
@@ -65,91 +68,80 @@ describe Host do
     it "returns hostname" do
       hostname = 'ghost-test-hostname.local'
 
-      Host.add(hostname)
-      host = Host.list.first
+      Ghost::HostsFile.add(hostname)
+      host = Ghost::HostsFile.list.first
       host.to_s.should eql(hostname)
     end
   end
 
   describe "finder methods" do
     before do
-      Host.add('abc.local')
-      Host.add('def.local')
-      Host.add('efg.local', '10.2.2.4')
+      Ghost::HostsFile.add('abc.local')
+      Ghost::HostsFile.add('def.local')
+      Ghost::HostsFile.add('efg.local', '10.2.2.4')
     end
 
-    it "returns valid Host when searching for host name" do
-      Host.find_by_host('abc.local').should be_instance_of(Host)
+    it "returns valid Ghost::HostsFile when searching for host name" do
+      Ghost::HostsFile.find_by_host('abc.local').should be_instance_of(Ghost::HostsFile)
     end
   end
 
   describe ".add" do
-    it "returns Host object when passed hostname" do
-      Host.add('ghost-test-hostname.local').should be_instance_of(Host)
+    it "returns Ghost::HostsFile object when passed hostname" do
+      Ghost::HostsFile.add('ghost-test-hostname.local').should be_instance_of(Ghost::HostsFile)
     end
 
-    it "returns Host object when passed hostname" do
-      Host.add('ghost-test-hostname.local', '10.0.0.2').should be_instance_of(Host)
+    it "returns Ghost::HostsFile object when passed hostname" do
+      Ghost::HostsFile.add('ghost-test-hostname.local', '10.0.0.2').should be_instance_of(Ghost::HostsFile)
     end
 
     it "raises error if hostname already exists and not add a duplicate" do
-      Host.empty!
-      Host.add('ghost-test-hostname.local')
-      lambda { Host.add('ghost-test-hostname.local') }.should raise_error
-      Host.list.should have(1).thing
+      Ghost::HostsFile.empty!
+      Ghost::HostsFile.add('ghost-test-hostname.local')
+      lambda { Ghost::HostsFile.add('ghost-test-hostname.local') }.should raise_error
+      Ghost::HostsFile.list.should have(1).thing
     end
 
     it "overwrites existing hostname if forced" do
       hostname = 'ghost-test-hostname.local'
 
-      Host.empty!
-      Host.add(hostname)
+      Ghost::HostsFile.empty!
+      Ghost::HostsFile.add(hostname)
 
-      Host.list.first.hostname.should eql(hostname)
-      Host.list.first.ip.should eql('127.0.0.1')
+      Ghost::HostsFile.list.first.hostname.should eql(hostname)
+      Ghost::HostsFile.list.first.ip.should eql('127.0.0.1')
 
-      Host.add(hostname, '10.0.0.1', true)
-      Host.list.first.hostname.should eql(hostname)
-      Host.list.first.ip.should eql('10.0.0.1')
+      Ghost::HostsFile.add(hostname, '10.0.0.1', true)
+      Ghost::HostsFile.list.first.hostname.should eql(hostname)
+      Ghost::HostsFile.list.first.ip.should eql('10.0.0.1')
 
-      Host.list.should have(1).thing
-    end
-    
-    it "should add a hostname using second hostname's ip" do
-      hostname = 'ghost-test-hostname.local'
-      alias_hostname = 'ghost-test-alias-hostname.local'
-
-      Host.empty!
-
-      Host.add(hostname)
-      Host.add(alias_hostname, hostname)
-
-      Host.list.last.ip.should eql(Host.list.first.ip)
+      Ghost::HostsFile.list.should have(1).thing
     end
 
     it "should raise SocketError if it can't find hostname's ip" do
-      Host.empty!
-      lambda { Host.add('ghost-test-alias-hostname.google', 'google') }.should raise_error(SocketError)
+      Ghost::HostsFile.empty!
+      lambda { Ghost::HostsFile.add('ghost-test-alias-hostname.google', 'google') }.should raise_error(SocketError)
     end
   end
 
   describe ".empty!" do
     it "empties the hostnames" do
-      Host.add('ghost-test-hostname.local') # add a hostname to be sure
-      Host.empty!
-      Host.list.should have(0).things
+      Ghost::HostsFile.add('ghost-test-hostname.local') # add a hostname to be sure
+      Ghost::HostsFile.empty!
+      Ghost::HostsFile.list.should have(0).things
     end
   end
 
   describe ".delete_matching" do
     it "deletes matching hostnames" do
       keep = 'ghost-test-hostname-keep.local'
-      Host.add(keep)
-      Host.add('ghost-test-hostname-match1.local')
-      Host.add('ghost-test-hostname-match2.local')
-      Host.delete_matching('match')
-      Host.list.should have(1).thing
-      Host.list.first.hostname.should eql(keep)
+      Ghost::HostsFile.add(keep)
+      Ghost::HostsFile.add('ghost-test-hostname-match1.local')
+      Ghost::HostsFile.add('ghost-test-hostname-match2.local')
+      Ghost::HostsFile.delete_matching('match')
+      Ghost::HostsFile.list.should have(1).thing
+      Ghost::HostsFile.list.first.hostname.should eql(keep)
     end
   end
+  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
 $TESTING=true
-$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
-require 'rubygems'
-require 'spec'
+#$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require 'shared_examples'
