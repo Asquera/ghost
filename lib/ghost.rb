679429168e26869076525785ac1c8dc814e093c8
Florian Gilcher <florian.gilcher@asquera.de>
2011-08-29 09:01:59 +0200

    Load new classes correctly

--- lib/ghost.rb
+++ lib/ghost.rb
@@ -2,9 +2,11 @@ $: << File.dirname(__FILE__)
 
 case RUBY_PLATFORM
 when /darwin/
-  require 'ghost/mac-host'
+  require 'ghost/dscl'
+  Ghost::Host.adapter = Ghost::Dscl
 when /linux/
-  require 'ghost/linux-host'
+  require 'ghost/hosts_file'
+  Ghost::Host.adapter = Ghost::HostsFile
 end
 
 require 'ghost/ssh_config'
